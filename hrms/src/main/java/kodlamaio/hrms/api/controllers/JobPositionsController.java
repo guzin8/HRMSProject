e29a0de6991ad5ab85108c5ce6be20f85948c031
guzin8 <kanburogluguzin@gmail.com>
2021-05-10 17:23:02 +0300

    Human Resource Manager Systems

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
@@ -0,0 +1,30 @@
+package kodlamaio.hrms.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import kodlamaio.hrms.business.abstracts.JobPositionService;
+import kodlamaio.hrms.entities.concretes.JobPosition;
+
+@RestController
+@RequestMapping("/api/jobpositions")
+public class JobPositionsController {
+	
+	private JobPositionService jobPositionService;
+
+	@Autowired
+	public JobPositionsController(JobPositionService jobPositionService) {
+		super();
+		this.jobPositionService = jobPositionService;
+	}
+	
+	@GetMapping("/getall")
+	public List<JobPosition> getAll(){
+		return this.jobPositionService.getAll();
+	}
+
+}
